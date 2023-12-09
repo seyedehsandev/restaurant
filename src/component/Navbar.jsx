6f6438f0d369952cf01a3097a492d0b2cdaacdfa
Ehsan <dev.ehsanhosseini@gmail.com>
2023-12-09 17:36:29 +0330

    dish down deleted

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -73,7 +73,7 @@ export default function Navbar() {
                 </a>
                 <a href="#" className="flex flex-col justify-center items-center gap-y-2 hover:bg-gray-700  w-full pt-4 text-center hover:text-orange-600">
                     
-                    <li className="hover:text-orange-600 flex gap-x-1 items-center " onClick={dishesHandler}>Dishes<FaAngleDown className="cursor-pointer" /></li>
+                    <li className="hover:text-orange-600 flex gap-x-1 items-center " onClick={dishesHandler}>Dishes</li>
                     {isDishedOpen ?
                     <ul className="flex flex-col rounded-lg w-full">
                         <li className='hover:text-orange-100 hover:bg-gray-800 w-full bg-gray-400 py-2'>item</li>
